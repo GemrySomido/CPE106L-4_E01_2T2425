5f21d7c94fa5f84abe78b67cc7b551f40e0bda88
GemrySomido <gcsomido@mymail.mapua.edu.ph>
2025-02-08 16:25:25 +0800

    added triangle.py

---
+++ triangle.py
@@ -0,0 +1,9 @@
+"""
+Write a program in a file names triangle.py to compute the area of a triangle. Issue the appropriate prompts for the triangle's
+base and height. Then, use the formula .5 * base * height to compute the area. Test the program for an IDLE window.
+"""
+
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+area = 0.5 * base * height
+print("Area:", area)
